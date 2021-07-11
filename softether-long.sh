ed8db8c5318e4ad1f8012df763fc594d6f9ca122
root <root@localhost.localdomain>
2021-07-11 14:11:48 +0500

    123

--- softether-long.sh
+++ softether-long.sh
@@ -3,9 +3,19 @@
 os_distribution="$(cat /etc/os-release | grep "ID" | head -1 | awk -F '"' '{print $2}')"
 
 if [[ "$os_distribution" == "centos" ]]; then
-  yum install -y epel-release
-  yum update -y
+  yum install -y epel-release && yum update -y
   yum install -y git readline-devel openssl-devel make gcc
 elif [[ "$os_distribution" != "centos" ]]; then
   apt install -y git make gcc
 fi
+
+cd /tmp
+git clone https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git
+cd SoftEtherVPN_Stable
+./configure
+make
+mv bin/vpnserver /opt
+mv systemd/softether-vpnserver.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable softether-vpnserver.service && systemctl start softether-vpnserver.service
+rm -rf /opt/SoftEtherVPN_Stable
