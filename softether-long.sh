5773e7b39e530e94da73ed4a4d8ccef1bacdbcca
Igor Pominov <ipominov@mail.ru>
2021-07-11 15:56:58 +0500

    Update softether-long.sh

--- softether-long.sh
+++ softether-long.sh
@@ -4,13 +4,10 @@
 # устанавливает пакеты, компилирует сервер из исходников, создает и запускает службы.
 
 # Берем в переменную название и версию дистрибутива:
-
 os_distrib="$(cat /etc/os-release | grep "ID" | head -1 | awk -F '"' '{print $2}')"
 distrib_version="$(cat /etc/os-release | grep "VERSION_ID" | head -1 | awk -F '"' '{print $2}')"
 
-
 # Устанавливаем пакеты в зависимости от дистирибутива
-
 if [[ "$os_distrib" == "centos" ]] && [[ "$distrib_version" == "7" ]]; then
   yum install -y epel-release && yum update -y
   yum install -y git readline-devel openssl-devel make gcc
@@ -24,25 +21,21 @@ if [[ "$os_distrib" == "ubuntu" ]]; then
 fi
 
 # Качаем исходники с гитхаба во временную папку:
-
 cd /tmp
 git clone https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git
 cd SoftEtherVPN_Stable
-# Компилируем:
 
+# Компилируем:
 ./configure
 make
 
 # перемещаем готовые бинарники и юниты:
-
 mv bin/vpnserver /opt
 mv systemd/softether-vpnserver.service /etc/systemd/system
 
 # Перечитываем юниты, активируем и запускаем службу:
-
 systemctl daemon-reload
 systemctl enable softether-vpnserver.service && systemctl start softether-vpnserver.service
 
 # Удаляем временную папку:
-
 rm -rf /opt/SoftEtherVPN_Stable
