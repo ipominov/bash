0cee8513efe2967714e93a0c9cc8912456918504
root <root@localhost.localdomain>
2021-07-11 14:29:55 +0500

    dnf

--- softether-long.sh
+++ softether-long.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Скрипт устанавливает SoftEther VPN Server на CentOS 7 и Ubuntu -
+# Скрипт устанавливает SoftEther VPN Server на CentOS и Ubuntu -
 # устанавливает пакеты, компилирует сервер из исходников, создает и запускает службы.
 
-# Берем название дистрибутива:
-os_distribution="$(cat /etc/os-release | grep "ID" | head -1 | awk -F '"' '{print $2}')"
+# Берем в переменную название и версию дистрибутива:
+os_distrib="$(cat /etc/os-release | grep "ID" | head -1 | awk -F '"' '{print $2}')"
+distrib_version="$(cat /etc/os-release | grep "VERSION_ID" | head -1 | awk -F '"' '{print $2}')"
 
 # Устанавливаем пакеты в зависимости от дистирибутива
 if [[ "$os_distribution" == "centos" ]]; then
-  yum install -y epel-release && yum update -y
-  yum install -y git readline-devel openssl-devel make gcc
+  if [[ "$distrib_version" == "7" ]]; then
+    yum install -y epel-release && yum update -y
+    yum install -y git readline-devel openssl-devel make gcc
+  elif [[ "$os_distribution" != "7" ]]; then
+    dnf update -y
+    dnf install -y git readline-devel openssl-devel make gcc
+  fi
 elif [[ "$os_distribution" != "centos" ]]; then
   apt install -y git make gcc
 fi
