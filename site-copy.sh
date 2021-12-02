c81a02aa7643496e6ed5d5199b0371a2f7ce1651
Igor Pominov <ipominov@mail.ru>
2021-12-02 12:40:26 +0500

    Create site-copy.sh

---
+++ site-copy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Укажи, откуда копировать (путь до каталога без слеша в конце)"
+read srcdir
+
+[ -d $srcdir ]
+
+  if [[ $? -eq 0 ]]; then 
+    shopt -s dotglob
+    cp -rp $srcdir/* ./
+
+    elif [[ ! $? -eq 0 ]]; then
+      echo "Каталог не существует";
+  fi
+
+[ -f wp-config.php ]
+
+  if [[ $? -eq 0 ]]; then
+
+    src_db_name="$(cat wp-config.php | grep "DB_NAME" | awk -F "'" '{print $4}')"
+    src_db_user="$(cat wp-config.php | grep "DB_USER" | awk -F "'" '{print $4}')"
+    src_db_password="$(cat wp-config.php | grep "DB_PASSWORD" | awk -F "'" '{print $4}')"
+
+  elif [[ ! $? -eq 0 ]]; then
+    echo "Файла конфигурации Wordpress нет";
+
+  fi
