64f625d37b5766f2526911a8f105a836bd13b9c8
Igor Pominov <ipominov@gmail.com>
2022-04-15 10:49:46 +0500

    Update k8s-install-ubuntu.sh

--- k8s-install-ubuntu.sh
+++ k8s-install-ubuntu.sh
@@ -20,5 +20,5 @@ EOF
 systemctl restart docker
 
 curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/ku>
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt update -y && apt install kubelet kubeadm kubectl -y
